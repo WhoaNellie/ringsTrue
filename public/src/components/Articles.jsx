9e61cbd16cf1708e3b2fd007a8a839d4887cda3f
Nellie <fabybacenelson@gmail.com>
2020-04-15 17:56:13 -0700

    read button functioning

--- public/src/components/Articles.jsx
+++ public/src/components/Articles.jsx
@@ -1,45 +1,57 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { UserContext } from '../App';
-import axios from 'axios';
+import React, { useEffect, useContext, useState } from "react";
+import { UserContext } from "../App";
+import axios from "axios";
 
-function Articles(){
-    const [articleState, setArticleState] = useState({
-        rated: [],
-        shown: [],
-        articleArr: []
-    })
-    const Login = useContext(UserContext);
+function Articles() {
+  const [articleState, setArticleState] = useState({
+    rated: [],
+    read: [],
+    articleArr: [],
+  });
+  const Login = useContext(UserContext);
 
-    useEffect( () => {
-        axios.get("/api/articles").then( (articles) => {
-            setArticleState({...articleState, articleArr: articles.data})
-            // for(let i = 0; i < 5 - Login[0].dailyRated; i++){
-            //     cardArr.push(<Card article={articleArr[i]}/>);
-            //     setArticleState({...articleState, shown: articleState.shown.push(i)});
-            // }
-        })
-    }, [])
+  useEffect(() => {
+    axios.get("/api/articles").then((articles) => {
+      setArticleState({ ...articleState, articleArr: articles.data });
+      // for(let i = 0; i < 5 - Login[0].dailyRated; i++){
+      //     cardArr.push(<Card article={articleArr[i]}/>);
+      //     setArticleState({...articleState, shown: articleState.shown.push(i)});
+      // }
+    });
+  }, []);
 
-    return(
+  return (
     <React.Fragment>
-        {articleState.articleArr.map( (article) => {
-            return <Card article={article}/>
-        })}
+      {articleState.articleArr.map((article) => {
+        return <Card article={article} />;
+      })}
     </React.Fragment>
-    )
+  );
 }
 
-function Card({ article }){
-    return (
-        <div className="card">
-            <h3>{article.headline}</h3>
-            <img src={article.image} alt={article.description}/>
-            <div className="article-text">{article.text}</div>
+function Card({ article }) {
+  const [cardState, setCardState] = useState({
+    height: "collapsed",
+  });
 
-            <button className="read-button">Read</button>
-            <button className="rateButton">Rate</button>
-        </div>
-    )
+  return (
+    <div className={`card article-${article.id} ${cardState.height}`}>
+      <h3>{article.headline}</h3>
+      <img src={article.image} alt={article.description} />
+      <div className="article-text">{article.text}</div>
+
+      {cardState.height === "collapsed" && (
+        <button
+          className={`read-button article-${article.id}`}
+          onClick={() => setCardState({height: 'full'})}
+        >
+            Read
+        </button>
+      )}
+
+      <button className="rateButton">Rate</button>
+    </div>
+  );
 }
 
 export default Articles;
