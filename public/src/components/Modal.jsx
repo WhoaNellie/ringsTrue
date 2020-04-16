7db5f0535045e4ea523c6cadddca49f493edbfcb
Nellie <fabybacenelson@gmail.com>
2020-04-16 16:11:34 -0700

    close and reset of modal done

--- public/src/components/Modal.jsx
+++ public/src/components/Modal.jsx
@@ -5,17 +5,19 @@ import { Doughnut } from 'react-chartjs-2';
 function Modal({isShowing, setIsShowing, headline}) {
     const modalRoot = document.createElement('div');
     modalRoot.setAttribute('id', 'modal-root');
-    document.body.append(modalRoot);
+    document.body.append(modalRoot);      
+    const [rangeState, setRangeState] = useState({accuracy: 50,
+    neutrality: 50});
     const [chartState, setChartState] = useState({
         datasets: [
           {
             label: "Accuracy",
-            data: [60, 40],
+            data: [rangeState.accuracy, 100-rangeState.accuracy],
             backgroundColor: ["#008000", "#FFFFFF"],
           },
           {
             label: "Neutrality",
-            data: [60, 40],
+            data: [rangeState.neutrality, 100-rangeState.neutrality],
             backgroundColor: ["#800080", "#FFFFFF"],
           },
         ],
@@ -25,36 +27,85 @@ function Modal({isShowing, setIsShowing, headline}) {
         tooltips: { enabled: false },
         hover: { mode: null },
       };
-      
 
-    function calcChart(val, chart){
-        let curData = chartState.datasets[chart];
+    function handleRange(e, chart){
+        setRangeState({...rangeState, [chart]:e.target.value});
+            if(chart === 'accuracy'){
+                setChartState({
+                    datasets: [
+                      {
+                        label: "Accuracy",
+                        data: [e.target.value, 100-e.target.value],
+                        backgroundColor: ["#008000", "#FFFFFF"],
+                      },
+                      {
+                        label: "Neutrality",
+                        data: [rangeState.neutrality, 100-rangeState.neutrality],
+                        backgroundColor: ["#800080", "#FFFFFF"],
+                      },
+                    ]
+                  })
+            }else if(chart === 'neutrality'){
+                setChartState({
+                    datasets: [
+                      {
+                        label: "Accuracy",
+                        data: [rangeState.accuracy, 100-rangeState.accuracy],
+                        backgroundColor: ["#008000", "#FFFFFF"],
+                      },
+                      {
+                        label: "Neutrality",
+                        data: [e.target.value, 100-e.target.value],
+                        backgroundColor: ["#800080", "#FFFFFF"],
+                      },
+                    ]
+                  })
+            }
+    }
 
-        curData.data = [val, 100-val];
+    function handleSubmit(){
+        handleClose();
 
-        let curDataSet = chartState.datasets;
+    }
 
-        curDataSet[chart] = curData;
-
-        setChartState({...chartState, datasets: curDataSet});
+    function handleClose(){
+        setIsShowing(false);
+        setRangeState({
+            accuracy: 50,
+            neutrality: 50
+        });
+        setChartState({
+            datasets: [
+              {
+                label: "Accuracy",
+                data: [50,50],
+                backgroundColor: ["#008000", "#FFFFFF"],
+              },
+              {
+                label: "Neutrality",
+                data: [50,50],
+                backgroundColor: ["#800080", "#FFFFFF"],
+              },
+            ],
+          });
     }
 
     if(isShowing) {
         return ReactDOM.createPortal(
             <React.Fragment>
-            <div className="mask" onClick={() => setIsShowing(false)}></div>
+            <div className="mask" onClick={() => handleClose()}></div>
             <aside className="modal rating">
                     <h3>How Do You Rate "{headline}"?</h3>
     
                     <Doughnut data={chartState} options={options}/>
     
                     <label htmlFor="accuracy">Accuracy</label>
-                    <input id="accuracy" name="accuracy" type="range" min="0" max="100" onChange={() => calcChart(document.getElementById("accuracy").value, 0)}/>
+                    <input id="accuracy" name="accuracy" type="range" min="0" max="100" value={rangeState.accuracy} onChange={(event) => handleRange(event, "accuracy")}/>
     
                     <label htmlFor="neutrality">Neutrality</label>
-                    <input id="neutrality" name="neutrality" type="range" min="0" max="100"/>
+                    <input id="neutrality" name="neutrality" type="range" min="0" max="100" value={rangeState.neutrality} onChange={(event) => handleRange(event, "neutrality")}/>
     
-                    <button className="submit" onClick={() => setIsShowing(false)}>Submit</button>
+                    <button className="submit" onClick={() => handleSubmit()}>Submit</button>
                 </aside>
             </React.Fragment>, modalRoot
     );
