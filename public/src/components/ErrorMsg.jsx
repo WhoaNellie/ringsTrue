50f2967782885933fb8f3d9f6800dc4670ef1119
Nellie <fabybacenelson@gmail.com>
2020-04-20 20:50:20 -0700

    err messages

---
+++ public/src/components/ErrorMsg.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ErrorMsg({ message, field }){
+    return(
+    <label htmlFor={field}><strong>{message}</strong></label>
+    )
+}
+
+export default ErrorMsg;
