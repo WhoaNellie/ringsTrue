e8b7418136aedad380b316adbd7e3093df8b9c68
Nellie <fabybacenelson@gmail.com>
2020-04-24 21:47:36 -0700

    fized class

--- public/src/pages/Homepage.jsx
+++ public/src/pages/Homepage.jsx
@@ -13,7 +13,7 @@ function Homepage(){
         <Search/>
     <main>
         <CookiesProvider>
-            <div {(!Login[0].loggedIn && "className='flex'")}>
+            <div className={!Login[0].loggedIn ? 'flex': null}>
             {!Login[0].loggedIn && <p className="blurb">ringsTrue is a news network rating app where anyone can be the judge. You will be asked to critique five anonymized articles a day. Then compare your rating to the network's overall ranking and to your preconcieved notions!</p>}
             {Login[0].loggedIn ? <Articles/>:<RorL/>}
             </div>
