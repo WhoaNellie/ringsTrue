462bcd683e2dbe7989b7bebf364e4191770b0481
nellie <NaN@nellienelson.com>
2023-08-21 16:20:45 -0700

    new dist

--- server.js
+++ server.js
@@ -14,41 +14,40 @@ const MongoStore = require("connect-mongo")(session);
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-let uristring =
-  process.env.DB_URI;
+let uristring = process.env.DB_URI;
 
-// let cookie_key = process.env.cookie_key;
+let cookie_key = process.env.cookie_key;
 
-// mongoose.connect(uristring, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(uristring, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
 //cookies
-// app.use(
-//   session({
-//     secret: cookie_key,
-//     resave: true,
-//     saveUninitialized: false,
-//     store: new MongoStore({
-//       mongooseConnection: db,
-//     }),
-//     cookie: {
-//       maxAge: null,
-//       httpOnly: false,
-//       secure: process.env.NODE_ENV === "production",
-//     },
-//   })
-// );
+app.use(
+  session({
+    secret: cookie_key,
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db,
+    }),
+    cookie: {
+      maxAge: null,
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+    },
+  })
+);
 
 app.use(logger("dev"));
 app.use(compression());
 app.use(express.json());
 app.use(express.static("public"));
-// app.use(cookieParser(cookie_key));
+app.use(cookieParser(cookie_key));
 
 // routes
 app.use(require("./routes/api-routes.js"));
